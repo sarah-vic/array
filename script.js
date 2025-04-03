1c8fb009acf5d73dcaf7689b4e80f0e409d963c0
Sarah <sah.winnermind@gmail.com>
2025-04-03 09:31:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+//JS-Javascript
+
+var nome = ["Luana", "Pedro","Rafaela","Mariana", "Kali", "Kailany", "Samuel", "Willian", "Pietro", "Vinicius","Laura","Sarah", "Pedro Hnerique", "Logan","Miguel", "Nicoli"]
+
+var nomeEscolhido = parseInt(prompt("Digite o número de 0 a 15"))
+
+document.write("Nome escolhido:" + nome[nomeEscolhido])
